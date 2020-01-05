13e3e990356313cbb24ea4631737954dc519de5e
David Glueck <33424164+dvglc@users.noreply.github.com>
2020-01-05 16:34:42 +0100

    Make footer links open in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,12 @@ import { faFileImage } from '@fortawesome/free-solid-svg-icons'
 const Footer = () => 
     <div className="Footer">
         <div className="about">{C.NAME} ({C.VERSION})</div>
-        <div className="source"><a href={C.LINK}><FontAwesomeIcon icon={faGithub} /> Source Code</a></div>
-        <div className="facs"><a href={C.FACS_LINK}><FontAwesomeIcon icon={faFileImage} /> Original Facsimile</a></div>
+        <div className="source">
+            <a href={C.LINK} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /> Source Code</a>
+        </div>
+        <div className="facs">
+            <a href={C.FACS_LINK} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFileImage} /> Original Facsimile</a>
+        </div>
     </div>
 
 

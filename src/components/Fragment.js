36a6630182d41f01288f6bfbc14af19b8e75c700
David Glueck <33424164+dvglc@users.noreply.github.com>
2020-01-12 17:25:39 +0100

    Highlighting through css animations

--- src/components/Fragment.js
+++ src/components/Fragment.js
@@ -10,37 +10,20 @@ const Fragment = ({store, fragment}) => {
         }  
         else {
             store.dispatch(misclick())
-            alert('Fragment is not movable!')
+            //alert('Fragment is not movable!')
         } 
     }
 
-    let _style = {
-        fill: 'green',
-        fillOpacity: (fragment.correct) ? 0.25 : 0
-    }
-
-    const highlight = e => {
-        if (fragment.movable) e.target.style.fill="blue" 
-        else e.target.style.fill="red" 
-        e.target.style.fillOpacity = 0.25
-    }
-    const lowlight = e => {
-        e.target.style.fill = 'green'
-        e.target.style.fillOpacity = (fragment.correct) ? 0.25 : 0
-    } 
+    const renderClass = (fragment.correct && fragment.movable) ? 'correctmovable' : (fragment.correct) ? 'correct' : (fragment.movable) ? 'movable' : 'none'
     
     const image = require('../assets/img/' + fragment.img) 
     // TODO decrease img file size (ideally, < 10000 bytes) for faster loading
 
-    // console.log("Building Fragment with id: " + fragment.id + " at position: " 
-    //     + JSON.stringify(fragment.position) + " with image " + fragment.img 
-    //     + " at @x=" + fragment.position.col + " and @y=" + fragment.position.row)
-
     return (
         <g className="Fragment">
             <image href={image} x={fragment.position.col} y={fragment.position.row} width="1" height="1"/>
-            <rect x={fragment.position.col} y={fragment.position.row} width="1" height="1" 
-                style={_style} onMouseOver={highlight} onMouseOut={lowlight} onClick={requestMove} />
+            <rect className={renderClass} x={fragment.position.col} y={fragment.position.row} width="1" height="1" 
+                 onClick={requestMove} />
         </g>
     )
 }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,10 +6,6 @@ import './Grid.css'
 
 const Grid = ({store}) => {
 
-    // console.log('Blank fragment is ' + store.getState().blank.id + ' at position ' + JSON.stringify(store.getState().blank))
-    // console.log('Movables are:')
-    // store.getState().fragments.filter(f => f.movable).map(f => console.log(f.id + ' at position ' + JSON.stringify(f.position)))
-
     // this is where we check the status of the game: when all fragments are correctly positioned, a success action is dispatched
     if (store.getState().status.correctFragments === store.getState().fragments.length) {
         alert('Game ended!')
@@ -18,7 +14,7 @@ const Grid = ({store}) => {
     return (
         <div className="Grid">
             <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
-                width={C.GRID_WIDTH} height={C.GRID_HEIGHT} viewBox={'0 0 ' + C.GRID_COLUMNS + ' ' + C.GRID_ROWS}>  
+                width={C.GRID_WIDTH} height={C.GRID_HEIGHT} viewBox={'0 0 ' + C.GRID_COLUMNS + ' ' + C.GRID_ROWS}>
                 {
                 store.getState().fragments.map((f,i) => <Fragment store={store} key={i} fragment={f} />)
                 }

e111b2bf25d786ca0e1b29372d1de51c24262b47
David Glueck <33424164+dvglc@users.noreply.github.com>
2020-03-10 19:38:25 +0100

    Reduce logging

--- src/store/index.js
+++ src/store/index.js
@@ -8,12 +8,12 @@ import { initializeState } from './initialState'
 
 const logger = store => next => action => {
     let result
-    console.groupCollapsed('dispatching', action.type)
-    console.log('prev state', store.getState())
-    console.log('action', action)
+    // console.groupCollapsed('dispatching', action.type)
+    // console.log('prev state', store.getState())
+    // console.log('action', action)
     result = next(action)
-    console.log('next state', store.getState())
-    console.groupEnd()
+    // console.log('next state', store.getState())
+    // console.groupEnd()
     return result
 }
 
